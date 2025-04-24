5721aae2582b78b3dcc9a3b9bb569e66e3855c9b
Jorge052011 <jimunozacuna+1@gmail.com>
2025-04-24 00:59:47 +0000

    segun

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,34 @@ alert(mensaje) */
 
 
 
-    let nota1 = prompt('ingrese la nota 1 :')
-    alert(nota1)
+    let nota1 = prompt('ingrese la nota 1 de html :')    
+    document.getElementById('nota1html').textContent = nota1
 
-    
 
+    let nota2 = prompt('ingrese la nota 2 de html :')    
+    document.getElementById('nota2html').textContent = nota2
+
+    let nota3 = prompt('ingrese la nota 3 de html:')    
+    document.getElementById('nota3html').textContent = nota3
+
+
+    let nota1css = prompt('ingrese la nota 1 de css:')    
+    document.getElementById('nota1css').textContent = nota1css
+
+
+    let nota2css = prompt('ingrese la nota 2 de css:')    
+    document.getElementById('nota2css').textContent = nota2css
+
+    let nota3css = prompt('ingrese la nota 3 de css :')    
+    document.getElementById('nota3css').textContent = nota3css
+
+
+    let nota1js = prompt('ingrese la nota 1 de js:')    
+    document.getElementById('nota1js').textContent = nota1js
+
+
+    let nota2js = prompt('ingrese la nota 2 de js :')    
+    document.getElementById('nota2js').textContent = nota2js
+
+    let nota3js = prompt('ingrese la nota 3 de js:')    
+    document.getElementById('nota3js').textContent = nota3js
